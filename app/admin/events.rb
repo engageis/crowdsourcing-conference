caabc206ff2f0e98935184297d323d3162d7ecf9
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-03-14 18:31:47 -0300

    Clearing the event and refactored

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -10,36 +10,4 @@ ActiveAdmin.register Event do
   end
 
   form :partial => 'form'
-
-  # controller do
-  #   def new
-  #     @event = Event.new
-  #     LOCALES.each do |lang|
-  #       @event.event_translations.find_or_initialize_by_locale(lang[0])
-  #     end
-  #   end
-  # end
-  
-  
-  
-  
-  
-  # actions :all
-  #   index do
-  #     column :text
-  #     default_actions
-  #   end
-  # 
-  #   filter :text
-  # 
-  #   form do |f|
-  #     f.inputs do
-  #       f.has_many :event_translations do |g|
-  #         g.input :locale, :as => :radio, :collection => LANGUAGES
-  #         g.input :text, :as => :text
-  #       end
-  #     end
-  #     f.buttons
-  #   end
-
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,15 +2,4 @@ class Event < ActiveRecord::Base
   translates :text
   has_many :event_translations
   accepts_nested_attributes_for :event_translations#, :allow_destroy => true
-  
-  
-  protected
-    def create_missing_translations
-      ADDITIONAL_LOCALES.each do |lang|
-        t = translations.find_by_locale(lang[0].to_s)
-        if t.nil?
-          translations.create(:locale => lang[0].to_s)
-        end
-      end
-    end
 end

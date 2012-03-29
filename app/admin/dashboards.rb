643019fd6174aba0ce525e5fd1812779ab46a044
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-03-28 21:43:34 -0300

    added the Admin Users and ajusted the Admin Dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,12 +1,20 @@
 ActiveAdmin::Dashboards.build do
   section "Recent Subscriptions" do
     table do
-      Subscription.order('created_at desc').limit(10).collect do |subscription|
+      thead
         tr
-          td link_to(subscription.name, admin_subscription_path(subscription))
-          td mail_to(subscription.email)
-          td subscription.created_at.strftime(t('time.formats.long'))
-      end
+          th :name
+          th :email
+          th :payment
+          th "Created at"
+      tbody
+        Subscription.order('created_at desc').limit(10).collect do |subscription|
+          tr
+            td link_to(subscription.name, admin_subscription_path(subscription))
+            td mail_to(subscription.email)
+            td { status_tag (subscription.paid ? "Confirmed" : "Pending"), (subscription.paid ? :ok : :error) }
+            td subscription.created_at.strftime(t('time.formats.long'))
+        end
     end
   end
 end

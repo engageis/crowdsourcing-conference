a93196a43caf6d3f63a9f30f846d14f2a4350940
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-03-30 14:24:26 -0300

    added the gmail_smtp file

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ccs12::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => "587",
-  :domain => "localhost.localdomain",
-  :authentication => :plain,
-  :user_name => ENV["GMAIL_SMTP_USER"],
-  :password => ENV["GMAIL_SMTP_PASSWORD"]
-}

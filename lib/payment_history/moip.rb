02015ba6cd5a4fd1f4195e0329ed6426812dc035
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-04-05 23:56:16 -0300

    removed the debugs

--- lib/payment_history/moip.rb
+++ lib/payment_history/moip.rb
@@ -10,11 +10,6 @@ module PaymentHistory
       begin
         @payment = Payment.find_by_key @params[:id_transacao]
         @payment.update_from_service
-        puts '----1) update_from_service'
-        puts "----2) #{@payment.moip_value} -------- #{@params[:valor].to_s}"
-        puts "----3) #{@params[:status_pagamento].to_i} ---- #{TransactionStatus::AUTHORIZED}"
-        puts "----4) #{@params[:status_pagamento].to_i} ---- #{TransactionStatus::FINISHED}"
-        
         if @payment.moip_value == @params[:valor].to_s
           @response_code = ResponseCode::SUCCESS
           if @params[:status_pagamento].to_i == TransactionStatus::AUTHORIZED || @params[:status_pagamento].to_i == TransactionStatus::FINISHED
@@ -29,7 +24,6 @@ module PaymentHistory
       rescue
         @response_code = ResponseCode::NOT_PROCESSED
       end
-      puts "---------- last here"
       self
     end
 
